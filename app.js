8a0e9ba73757d57a281672a28b88ca4a168a447d
Timothy Tran <timotran@gmail.com>
2017-03-13 15:14:11 -0700

    Add for loop lines in the objects

--- app.js
+++ app.js
@@ -4,35 +4,48 @@ var firstAndPike = {
   minCust: 23;
   maxCust: 65;
   avgCookieSale: 6.3;
-  totalSale[i]:
-
+  for (var i = 0; i < 15; i++) {
+    var randonNum = Math.floor((Math.random() * 10) + 1);
+    totalSale[i] = randonNum * avgCookieSale;
+  }
 }
 
 var seaTacAirport = {
   minCust: 3;
   maxCust: 24;
   avgCookieSale: 1.2;
-  totalSale[i]:
+  for (var i = 0; i < 15; i++) {
+    var randonNum = Math.floor((Math.random() * 10) + 1);
+    totalSale[i] = randonNum * avgCookieSale;
+  }
 }
 
 var seattleCenter = {
   minCust: 11;
   maxCust: 38;
   avgCookieSale: 3.7;
-  totalSale[i];
-
+  for (var i = 0; i < 15; i++) {
+    var randonNum = Math.floor((Math.random() * 10) + 1);
+    totalSale[i] = randonNum * avgCookieSale;
+  }
 }
 
 var capitalHill = {
   minCust: 20;
   maxCust: 38;
-  totalSale: 2.3;
-
+  avgCookieSale: 2.3;
+  for (var i = 0; i < 15; i++) {
+    var randonNum = Math.floor((Math.random() * 10) + 1);
+    totalSale[i] = randonNum * avgCookieSale;
+  }
 }
 
 var alki = {
   minCust: 2;
   maxCust: 16;
   avgCookieSale: 4.6;
-  totalSale[i];
+  for (var i = 0; i < 15; i++) {
+    var randonNum = Math.floor((Math.random() * 10) + 1);
+    totalSale[i] = randonNum * avgCookieSale;
+  }
 }
