5d9555186a3c228ec6eaa5d9bedc5c3658736c30
Timothy Tran <timotran@gmail.com>
2017-03-13 14:38:10 -0700

    First commit of index.html app.js style.css

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var firstAndPike = {
+  minCust: 23;
+  maxCust: 65;
+  avgCookieSale: 6.3;
+  totalSale[i]:
+
+}
+
+var seaTacAirport = {
+  minCust: 3;
+  maxCust: 24;
+  avgCookieSale: 1.2;
+  totalSale[i]:
+}
+
+var seattleCenter = {
+  minCust: 11;
+  maxCust: 38;
+  avgCookieSale: 3.7;
+  totalSale[i];
+
+}
+
+var capitalHill = {
+  minCust: 20;
+  maxCust: 38;
+  totalSale: 2.3;
+
+}
+
+var alki = {
+  minCust: 2;
+  maxCust: 16;
+  avgCookieSale: 4.6;
+  totalSale[i];
+}
