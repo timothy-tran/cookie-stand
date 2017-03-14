e933997c8acb68af26514fe50f8c9eb5098eeb81
Timothy Tran <timotran@gmail.com>
2017-03-14 14:25:54 -0700

    Fixed bugs in app.js

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
 'use strict';
 
-var openingHours = 15;
+var openingHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+
+var body = document.getElementsByTagName('body')[0];
 
 var firstAndPike = {
   minCust: 23,
   maxCust: 65,
   avgCookieSale: 6.3,
+  storeLocation: '1st and Pike',
+  saleEachHour: [],
   randomCust: function () {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    return Math.floor(Math.random() * (  ((this.maxCust - this.minCust) + 1) - this.minCust));
   },
-  saleEachHour: function () {
-    var totalSaleByHour = [];
-    for (var i = 0; i < openingHours; i++) {
-      totalSaleByHour.push(Math.round(this.randomCust() * this.avgCookieSale));
+  salePerDay: function () {
+    var totalCookies = 0;
+    for (var i = 0; i < openingHours.length -1; i++) {
+      this.saleEachHour.push(Math.round(this.randomCust() * this.avgCookieSale));
+      totalCookies += this.saleEachHour[i];
     }
-    return totalSaleByHour;
+    this.saleEachHour.push(totalCookies);
+    console.log(this.saleEachHour);
+  },
+  creatListItems: function() {
+    this.salePerDay();
+    var storeName = document.createElement('h2');
+    storeName.textContent = this.storeLocation;
+    body.appendChild(storeName);
+    var storeList = document.createElement('ul');
+    for (var i = 0; i < openingHours.length; i++) {
+      var hourList = document.createElement('li');
+      hourList.textContent = openingHours[i] + ': ' + this.saleEachHour[i] + ' cookies.';
+      storeList.appendChild(hourList);
+    };
+  //  console.log(hourList);
+
+    body.appendChild(storeList);
   }
 }
 
-var seaTacAirport = {
+firstAndPike.creatListItems();
+
+/*var seaTacAirport = {
   minCust: 3,
   maxCust: 24,
   avgCookieSale: 1.2,
@@ -80,4 +103,4 @@ var alki = {
     }
     return totalSaleByHour;
   }
-}
+}*/
