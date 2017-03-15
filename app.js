121510e3d33759462a5ed99d0c9271b5d514bb2c
Timothy Tran <timotran@gmail.com>
2017-03-15 14:20:34 -0700

    add the table head to app.js and add some styling to css file

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 'use strict';
 
-var openingHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+var openingHours = [' ', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var storeName = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capital Hill', 'Alki'];
 var body = document.getElementsByTagName('body')[0];
+var myTable = document.createElement('table');
 
-function cookiesStore (min, max, avg, location) {
+function tableHeading () {
+  var tableHead = document.createElement('thead');
+  var tableRow = document.createElement('tr');
+  for (var i = 0; i < openingHours.length; i++) {
+    var storeHour = document.createElement('th');
+    storeHour.textContent = openingHours[i];
+    tableRow.appendChild(storeHour);
+  }
+  tableHead.appendChild(tableRow);
+  myTable.appendChild(tableHead);
+  body.appendChild(myTable);
+}
+
+tableHeading();
+/*
+function CookiesStore (min, max, avg, location) {
   this.minCust = min;
   this.maxCust = max;
   this.avgCookieSale = avg;
@@ -25,26 +41,28 @@ function cookiesStore (min, max, avg, location) {
   };
   this.creatListItems = function() {
     this.salePerDay();
-    var storeName = document.createElement('h2');
-    storeName.textContent = this.storeLocation;
-    body.appendChild(storeName);
-    var storeList = document.createElement('ul');
+
+    var storeList = document.createElement('tr');
     for (var i = 0; i < openingHours.length; i++) {
-      var hourList = document.createElement('li');
-      hourList.textContent = openingHours[i] + ': ' + this.saleEachHour[i] + ' cookies.';
+      var storeName = document.createElement('th');
+      storeName.textContent = this.storeName;
+      body.appendChild(storeName);
+      var hourList = document.createElement('td');
+      hourList.textContent = openingHours[i] + ': ' + this.saleEachHour[i];
       storeList.appendChild(hourList);
     };
     body.appendChild(storeList);
   }
 }
 
-var firstAndPike = new cookiesStore(23, 65, 6.3, storeName[0]);
-var seaTacAirport = new cookiesStore(3, 24, 1.2, storeName[1]);
-var seattleCenter = new cookiesStore(11, 38, 3.7, storeName[2]);
-var capitalHill = new cookiesStore(20, 38, 2.3, storeName[3]);
-var alki = new cookiesStore(2, 16, 4.6, storeName[4]);
+var firstAndPike = new CookiesStore(23, 65, 6.3, storeName[0]);
+var seaTacAirport = new CookiesStore(3, 24, 1.2, storeName[1]);
+var seattleCenter = new CookiesStore(11, 38, 3.7, storeName[2]);
+var capitalHill = new CookiesStore(20, 38, 2.3, storeName[3]);
+var alki = new CookiesStore(2, 16, 4.6, storeName[4]);
 firstAndPike.creatListItems();
 seaTacAirport.creatListItems();
 seattleCenter.creatListItems();
 capitalHill.creatListItems();
 alki.creatListItems();
+*/
